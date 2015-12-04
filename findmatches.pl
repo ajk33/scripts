565f591708a1402b0a7e857d825b18792ce6167c
Adam Kearns <akearns@yahoo.com>
2015-12-03 22:06:46 -0500

    Looks like I finished the program, not sure it ever worked before.

--- findmatches.pl
+++ findmatches.pl
@@ -2,24 +2,48 @@
 #
 
 my %hashmap;
-$drive1="/cygdrive/n";
-$drive2="/cygdrive/g";
+my @rmlist;
 
-open(FILE,"<$drive1/digest.txt");
+open(FILE,"</cygdrive/g/digest1.txt");
 readfile();
 close(FILE);
-open(FILE,"<$drive2/digest.txt");
+open(FILE,"</cygdrive/g/digest2.txt");
 readfile();
 close(FILE);
 
-foreach $key (sort keys %hashmap) {
-  print "$key $hashmap{$key}\n";
+$totsize=0;
+foreach $hash (sort keys %hashmap) {
+    $filenames=$hashmap{$hash};
+    $output="$hash$hashmap{$hash}";
+    @files=split(/,/ ,$output);
+    $numfiles=@files;
+#    print "$output $numfiles\n";
+    $lastdate=0;
+    while ($output =~ m#/(20\d+)/#g)  {
+       $date=$1;
+       if ($date != $lastdate && $lastdate != 0) {
+         #print "Date Mismatch $lastdate $date\n";
+       }
+       $lastdate=$date;
+    }
+    if ($numfiles == 3) {
+        ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+            $atime,$mtime,$ctime,$blksize,$blocks)
+               = stat("/cygdrive/g/".$files[2]);
+        $totsize += $size;
+        push(@rmlist,"\"$files[2]\"   # ".$files[1]." $size");
+    }
 }
 
+print "#!/bin/bash\n\n";
+foreach $file (sort @rmlist) {
+  print "rm $file\n";
+}
+print "# Saved $totsize bytes\n";
 sub readfile() {
   while (<FILE>) {
     chomp;
-    ($filename,$hash)=/SHA1\((.*?)\)= (.*)/;
-    $hashmap{$hash}.=$filename.",";
+    ($hash,$filename)=/(.*?) .(.*)/;
+    $hashmap{$hash}.=",".$filename;
   }
 }
