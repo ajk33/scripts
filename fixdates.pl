748f8f376708d6a20ce2b7317f133dac7fdc3a5e
Adam Kearns <akearns@yahoo.com>
2017-04-16 12:29:33 -0400

    Another thing that needed fixing.
    I don't remember the problem this used to fix, so I just reused
    the script to fix this problem.

--- fixdates.pl
+++ fixdates.pl
@@ -4,12 +4,34 @@
 #
 use 5.012; # so readdir assigns to $_ in a lone while test
 use File::Find;
+use Time::Local;
 
-my $dir = "/cygdrive/g/HDCamcorder";
 
+#my $dir = "/cygdrive/g/HDCamcorder";
+my $dir = "/cygdrive/r/DCIM/101MSDCF";
+
+my $incorrecttime = timelocal(0,40,17,20,1,107);
+my $correcttime = timelocal(36,05,13,5,2,117);
 find (\&modtime,$dir);
 
 sub modtime() {
+  (my $dev,my $ino,my $mode,my $nlink,my $uid,my $gid,my $rdev,my $size,
+    my $atime,my $mtime,my $ctime,my $blksize,my $blocks) = stat($File::Find::name);
+  
+  if ($ctime < $incorrecttime) {
+    print "$File::Find::name $ctime\t";
+    my $delta = $ctime - $incorrecttime;
+    my $newtime = $ctime - $incorrecttime + $correcttime;
+    print "$delta\t$newtime\t";
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($newtime);
+    $mon+=1; $year +=1900;
+    print "$hour:$min:$sec $year/$mon/$mday\n";
+    my @arr = ($File::Find::name);
+    utime $newtime,$newtime, @arr;
+  }
+}
+
+__DATA__
   my $dirtofix = $File::Find::name;
   if (! -d $dirtofix) {
     return;
